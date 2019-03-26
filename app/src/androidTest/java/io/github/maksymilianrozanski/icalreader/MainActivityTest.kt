ee0dc3b119145b7cfc3ded3be22496a3ca1d979c
Max <maksymilianrozanski@gmail.com>
2019-03-26 17:56:10 +0100

    add espresso contrib dependency, add espresso test

--- app/src/androidTest/java/io/github/maksymilianrozanski/icalreader/MainActivityTest.kt
+++ app/src/androidTest/java/io/github/maksymilianrozanski/icalreader/MainActivityTest.kt
@@ -1,12 +1,23 @@
 package io.github.maksymilianrozanski.icalreader
 
+import android.support.annotation.NonNull
 import android.support.test.InstrumentationRegistry
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.contrib.RecyclerViewActions.scrollToPosition
+import android.support.test.espresso.matcher.BoundedMatcher
+import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.rule.ActivityTestRule
+import android.support.v7.widget.RecyclerView
+import android.view.View
 import io.github.maksymilianrozanski.icalreader.component.DaggerTestAppComponent
 import io.github.maksymilianrozanski.icalreader.module.AppModule
 import io.github.maksymilianrozanski.icalreader.module.NetworkModule
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
+import org.hamcrest.CoreMatchers.containsString
+import org.hamcrest.Description
+import org.hamcrest.Matcher
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -42,13 +53,100 @@ class MainActivityTest {
 
         app.appComponent = testAppComponent
         testAppComponent.inject(this)
-
     }
 
     @Test
     fun integrationTest() {
-        //TODO: verify displayed data
         activityRule.launchActivity(null)
-        Thread.sleep(2000)
+        Thread.sleep(500)
+        onView(withId(R.id.recyclerViewId)).perform(scrollToPosition<RecyclerView.ViewHolder>(0))
+            .check(matches(atPosition(0, hasDescendant(withText("Informatyka Lab")))))
+        onView(withId(R.id.recyclerViewId))
+            .check(matches(atPosition(0, hasDescendant(withText(containsString("14:40"))))))
+        onView(withId(R.id.recyclerViewId))
+            .check(matches(atPosition(0, hasDescendant(withText(containsString("16:10"))))))
+        onView(withId(R.id.recyclerViewId))
+            .check(
+                matches(
+                    atPosition(
+                        0,
+                        hasDescendant(withText(containsString("Sala: San Francisco, Wykładowca: Pierwszy Wykładowca, Kod zajęć: IT 150 Lab 10, Last update: Fri Mar 15 17:38:47 UTC 2019")))
+                    )
+                )
+            )
+        onView(withId(R.id.recyclerViewId))
+            .check(matches(atPosition(0, hasDescendant(withText(containsString("św. Filipa 17 Kraków"))))))
+
+        onView(withId(R.id.recyclerViewId)).perform(scrollToPosition<RecyclerView.ViewHolder>(1))
+            .check(matches(atPosition(1, hasDescendant(withText("Historia Wyk")))))
+        onView(withId(R.id.recyclerViewId))
+            .check(matches(atPosition(1, hasDescendant(withText(containsString("08:00"))))))
+        onView(withId(R.id.recyclerViewId))
+            .check(matches(atPosition(1, hasDescendant(withText(containsString("09:30"))))))
+        onView(withId(R.id.recyclerViewId))
+            .check(
+                matches(
+                    atPosition(
+                        1,
+                        hasDescendant(withText(containsString("Sala: Praga, Wykładowca: Drugi Wykładowca, Kod zajęć: IT 152 Wyk 2, Last update: Fri Mar 15 17:38:47 UTC 2019")))
+                    )
+                )
+            )
+        onView(withId(R.id.recyclerViewId))
+            .check(matches(atPosition(1, hasDescendant(withText(containsString("św. Filipa 17 Kraków"))))))
+
+        onView(withId(R.id.recyclerViewId)).perform(scrollToPosition<RecyclerView.ViewHolder>(2))
+            .check(matches(atPosition(2, hasDescendant(withText("Chemia")))))
+        onView(withId(R.id.recyclerViewId))
+            .check(matches(atPosition(2, hasDescendant(withText(containsString("16:20"))))))
+        onView(withId(R.id.recyclerViewId))
+            .check(matches(atPosition(2, hasDescendant(withText(containsString("17:50"))))))
+        onView(withId(R.id.recyclerViewId))
+            .check(
+                matches(
+                    atPosition(
+                        2,
+                        hasDescendant(withText(containsString("Sala: Praga, Wykładowca: Trzeci Wykładowca, Kod zajęć: IT 200 Wyk 3, Last update: Fri Mar 15 17:38:47 UTC 2019")))
+                    )
+                )
+            )
+        onView(withId(R.id.recyclerViewId))
+            .check(matches(atPosition(2, hasDescendant(withText(containsString("św. Filipa 17 Kraków"))))))
+
+        onView(withId(R.id.recyclerViewId)).perform(scrollToPosition<RecyclerView.ViewHolder>(3))
+            .check(matches(atPosition(3, hasDescendant(withText("Biologia Ćw")))))
+        onView(withId(R.id.recyclerViewId))
+            .check(matches(atPosition(3, hasDescendant(withText(containsString("08:00"))))))
+        onView(withId(R.id.recyclerViewId))
+            .check(matches(atPosition(3, hasDescendant(withText(containsString("09:30"))))))
+        onView(withId(R.id.recyclerViewId))
+            .check(
+                matches(
+                    atPosition(
+                        3,
+                        hasDescendant(withText(containsString("Sala: Warszawa, Wykładowca: Czwarty Wykładowca, Kod zajęć: IT 220 Ćw 7, Last update: Fri Mar 15 17:38:47 UTC 2019")))
+                    )
+                )
+            )
+        onView(withId(R.id.recyclerViewId))
+            .check(matches(atPosition(3, hasDescendant(withText(containsString("św. Filipa 17 Kraków"))))))
+    }
+
+    //TODO: move to another class
+    fun atPosition(position: Int, @NonNull itemMatcher: Matcher<View>): Matcher<View> {
+        checkNotNull(itemMatcher)
+        return object : BoundedMatcher<View, RecyclerView>(RecyclerView::class.java) {
+            override fun describeTo(description: Description) {
+                description.appendText("has item at position $position: ")
+                itemMatcher.describeTo(description)
+            }
+
+            override fun matchesSafely(view: RecyclerView): Boolean {
+                val viewHolder = view.findViewHolderForAdapterPosition(position)
+                    ?: // has no item on such position
+                    return false
+                return itemMatcher.matches(viewHolder.itemView)
+            }
+        }
     }
 }
