7c90a00e63258d31399bb2ddbd79bdc38b0c56d8
Max <maksymilianrozanski@gmail.com>
2019-04-27 20:30:39 +0200

    add input validation methods: url with spaces, url ended with '.'

--- app/src/main/java/io/github/maksymilianrozanski/icalreader/data/CalendarForm.kt
+++ app/src/main/java/io/github/maksymilianrozanski/icalreader/data/CalendarForm.kt
@@ -2,26 +2,59 @@ package io.github.maksymilianrozanski.icalreader.data
 
 class CalendarForm(calendarName: String, calendarUrl: String) {
 
-    var calendarName:String = calendarName
+    var calendarName: String = calendarName
         set(value) {
             field = value
-            if (value.isNotEmpty()) {
-                nameError = null
-            } else {
-                nameError = "Cannot be blank"
-            }
+            isNameNotEmpty()
         }
 
     var calendarUrl = calendarUrl
         set(value) {
             field = value
-            if (value.isNotEmpty()) {
-                urlError = null
-            } else {
-                urlError = "Cannot be blank"
-            }
+            //first error from the left is set
+            isUrlNotEmpty() && isUrlWithoutSpaces() && isUrlNotEndedWithDot()
         }
 
     var nameError: String? = null
     var urlError: String? = null
+
+    fun isNameNotEmpty(): Boolean {
+        return if (calendarName.isNotEmpty()) {
+            nameError = null
+            true
+        } else {
+            nameError = "Cannot be blank"
+            false
+        }
+    }
+
+    fun isUrlNotEmpty(): Boolean {
+        return if (calendarUrl.isNotEmpty()) {
+            urlError = null
+            true
+        } else {
+            urlError = "Cannot be blank"
+            false
+        }
+    }
+
+    fun isUrlWithoutSpaces(): Boolean {
+        return if (!calendarUrl.contains(" ")) {
+            urlError = null
+            true
+        } else {
+            urlError = "Cannot contain spaces"
+            false
+        }
+    }
+
+    fun isUrlNotEndedWithDot():Boolean {
+        return if (calendarUrl.endsWith(".")) {
+            urlError = "Cannot end with '.'"
+            false
+        } else {
+            urlError = null
+            true
+        }
+    }
 }
--- app/src/test/java/io/github/maksymilianrozanski/icalreader/data/CalendarFormTest.kt
+++ app/src/test/java/io/github/maksymilianrozanski/icalreader/data/CalendarFormTest.kt
@@ -39,4 +39,91 @@ class CalendarFormTest {
         Assert.assertTrue(calendarForm.nameError == null)
         Assert.assertTrue(calendarForm.urlError == null)
     }
+
+    @Test
+    fun nameNotEmptyTest() {
+        val name = "name"
+        val url = "http://example.com"
+        val calendarForm = CalendarForm(name, url)
+
+        Assert.assertTrue(calendarForm.isNameNotEmpty())
+        Assert.assertTrue(calendarForm.nameError == null)
+    }
+
+    @Test
+    fun nameEmptyTest() {
+        val invalidName = ""
+        val url = "http://example.com"
+        val calendarForm = CalendarForm(invalidName, url)
+
+        Assert.assertFalse(calendarForm.isNameNotEmpty())
+        Assert.assertTrue(calendarForm.nameError == "Cannot be blank")
+    }
+
+    @Test
+    fun urlNotEmptyTest() {
+        val name = "name"
+        val url = "http://example.com"
+        val calendarForm = CalendarForm(name, url)
+
+        Assert.assertTrue(calendarForm.isUrlNotEmpty())
+        Assert.assertTrue(calendarForm.urlError == null)
+    }
+
+    @Test
+    fun urlEmptyTest() {
+        val name = "name"
+        val invalidUrl = ""
+        val calendarForm = CalendarForm(name, invalidUrl)
+
+        Assert.assertFalse(calendarForm.isUrlNotEmpty())
+        Assert.assertTrue(calendarForm.urlError == "Cannot be blank")
+    }
+
+    @Test
+    fun urlWithoutSpacesTest() {
+        val name = "name"
+        val url = "http://example.com"
+        val calendarForm = CalendarForm(name, url)
+        Assert.assertTrue(calendarForm.isUrlWithoutSpaces())
+        Assert.assertTrue(calendarForm.urlError == null)
+    }
+
+    @Test
+    fun urlWithSpacesTest() {
+        val name = "name"
+        val invalidUrl = "http://invalid url.com"
+        val calendarForm = CalendarForm(name, invalidUrl)
+
+        Assert.assertFalse(calendarForm.isUrlWithoutSpaces())
+        Assert.assertTrue(calendarForm.urlError == "Cannot contain spaces")
+    }
+
+    @Test
+    fun urlEndedWithDotTest(){
+        val name = "name"
+        val invalidUrl = "http://example.com."
+        val calendarForm = CalendarForm(name, invalidUrl)
+
+        Assert.assertFalse(calendarForm.isUrlNotEndedWithDot())
+        Assert.assertTrue(calendarForm.urlError == "Cannot end with '.'")
+    }
+
+    @Test
+    fun noErrorsValuesPassedByConstructorTest() {
+        val calendarForm = CalendarForm("", "")
+        Assert.assertTrue(calendarForm.nameError == null && calendarForm.urlError == null)
+    }
+
+    @Test
+    fun urlErrorMessageOrderTest() {
+        val name = "name"
+        val validUrl = "http://example.com"
+        val calendarForm = CalendarForm(name, validUrl)
+        Assert.assertTrue(calendarForm.nameError == null && calendarForm.urlError == null)
+
+        val withSpacesAndDot = "http://example .com."
+        calendarForm.calendarUrl = withSpacesAndDot
+        Assert.assertTrue(calendarForm.urlError == "Cannot contain spaces")
+    }
 }
