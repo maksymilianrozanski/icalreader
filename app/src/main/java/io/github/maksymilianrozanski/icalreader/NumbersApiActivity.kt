5aed0c99477edabc66511ae4ba4fceab2b55492c
Max <maksymilianrozanski@gmail.com>
2020-04-25 11:56:07 +0200

    add numbers api activity

---
+++ app/src/main/java/io/github/maksymilianrozanski/icalreader/NumbersApiActivity.kt
@@ -0,0 +1,22 @@
+package io.github.maksymilianrozanski.icalreader
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_numbers_api.*
+
+class NumbersApiActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_numbers_api)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
